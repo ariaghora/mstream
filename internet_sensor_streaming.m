96026dd64f51cdce2ae5e0bcc9c4e47837817ff7
Aria Ghora Prabono <hello@ghora.net>
2016-03-24 03:26:35 +0900

    implement internet streaming

--- internet_sensor_streaming.m
+++ internet_sensor_streaming.m
@@ -1,10 +1,12 @@
 clc;
-%input('Press enter to continue');
+disp('Stream sensor data from internet');
+input('Press enter to continue...');
 count = 1;
 while 1  
-    pause(1);
+    pause(0.5);
     count = count + 1;
-    s = urlread('http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo');
+    s = urlread('http://ghora.net/smartageing/getsensordata.php');
     json = parse_json(s);
-    disp(json{1}.geonames{count}.toponymName);
+    clc;
+    disp(json{1});
 end
